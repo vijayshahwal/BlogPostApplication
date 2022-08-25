93800bde0005e36933f5cdfb8386d44e3e78254c
vijayshahwal <13vijay12@gmail.com>
2022-08-25 23:03:51 +0530

    First Commit

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -136,10 +136,3 @@ import os
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 MEDIA_URL = '/media/'
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = '13vijay12@gmail.com' 
-EMAIL_HOST_PASSWORD = 'yhnvgjdcrsxafukd'
